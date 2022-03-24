9b51ffe9678c62013df601adfa57fd7c284dce95
Jordan Chavez <jordanchavez151@gmail.com>
2022-03-23 20:55:17 -0700

    agregamos linea

--- prueba.java
+++ prueba.java
@@ -2,6 +2,7 @@ class prueba{
 	
 	public static void main(String[] args){
 		System.out.println("Esta es la practica 2 de jenkins, Saludos");
+		System.out.println("La conexion con github se ha establecido");
 	}
 	
 }
